18079bdab81a3f99af729403a68900c663c61764
mitschix <23629789+mitschix@users.noreply.github.com>
2026-02-10 19:31:27 +0100

    refactor: cleanup and format file

--- alphaspelling_bot.py
+++ alphaspelling_bot.py
@@ -4,13 +4,8 @@
 #
 # Desc: Telegram bot that returns the spelling alphabet to a given string
 #
-# Notes:
-#   + the token can be placed in tel_token.py to import it
-#
 # Requires:
 #   + token: A telegram token from the @BotFather
-#   + python-telegram-bot: python library (currently Version 11.1.0)
-#
 
 
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
@@ -18,7 +13,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, \
     Filters, CallbackQueryHandler, ConversationHandler
 
 from tel_tokens import token
-
 
 # set conversation handler vars
 GER, INTER = range(2)
@@ -37,165 +31,157 @@ keyboard_after = [
 
 def get_mapping(word: str, lang: str) -> str:
     german_spelling = {
-        "A":    "Anton",
-        "B":    "Berta",
-        "C":    "Cäsar",
-        "D":    "Dora",
-        "E":    "Emil",
-        "F":    "Friedrich",
-        "G":    "Gustav",
-        "H":    "Heinrich",
-        "I":    "Ida",
-        "J":    "Julius",
-        "K":    "Kaufmann",
-        "L":    "Ludwig",
-        "M":    "Martha",
-        "N":    "Nordpol",
-        "O":    "Otto",
-        "P":    "Paula",
-        "Q":    "Quelle",
-        "R":    "Richard",
-        "S":    "Samuel",
-        "SCH":  "Schule",
-        "T":    "Theodor",
-        "U":    "Ulrich",
-        "V":    "Viktor",
-        "W":    "Wilhelm",
-        "X":    "Xanthippe",
-        "Y":    "Ypsilon",
-        "Z":    "Zacharias",
-        "Ä":    "Ärger",
-        "Ö":    "Ökonom",
-        "Ü":    "Übermut",
-
-        "0":    "Null",
-        "1":    "Eins",
-        "2":    "Zwei",
-        "3":    "Drei",
-        "4":    "Vier",
-        "5":    "Fünf",
-        "6":    "Sechs",
-        "7":    "Sieben",
-        "8":    "Acht",
-        "9":    "Neun",
-
-        " ":   "Leerzeichen",
-        ".":   "Punkt",
-        ",":   "komma",
-        ";":   "Semikolon",
-        ":":   "DoppelPunkt",
-        "?":   "Fragezeichen",
-        "!":   "Ausrufezeichen",
-        "@":   "At-Zeichen",
-        "&":   "Und-Zeichen",
-        "\"":  "Anführungszeichen",
-        "\'":  "Apostroph",
-        "-":   "Bindestrich",
-        "/":   "Schrägstrich",
-        "\\":  "Umgekehrter-Schrägstrich",
-        "(":   "Runde-Klammer-links",
-        ")":   "Runde-Klammer-rechts",
-        "[":   "Eckige-Klammer-links",
-        "]":   "Eckige-Klammer-rechts",
-        "{":   "Geschweifte-Klammer-links",
-        "}":   "Geschweifte-Klammer-rechts",
-        "<":   "Kleiner-als-Zeichen",
-        ">":   "Größer-als-Zeichen",
-        "|":   "Senkrechter-Strich",
-        "°":   "Gradzeichen",
-        "*":   "Sternchen",
-        "+":   "Pluszeichen",
-        "=":   "Gleichheitszeichen",
-        "#":   "Rautenzeichen",
-        "§":   "Paragraphenzeichen",
-        "$":   "Dollarzeichen",
-        "€":   "Euro-Zeichen",
-        "~":   "Tilde",
-        "_":   "Unterstrich",
-        "%":   "Prozentzeichen",
-        "^":   "Zirkumflex",
+        "A": "Anton",
+        "B": "Berta",
+        "C": "Cäsar",
+        "D": "Dora",
+        "E": "Emil",
+        "F": "Friedrich",
+        "G": "Gustav",
+        "H": "Heinrich",
+        "I": "Ida",
+        "J": "Julius",
+        "K": "Kaufmann",
+        "L": "Ludwig",
+        "M": "Martha",
+        "N": "Nordpol",
+        "O": "Otto",
+        "P": "Paula",
+        "Q": "Quelle",
+        "R": "Richard",
+        "S": "Samuel",
+        "SCH": "Schule",
+        "T": "Theodor",
+        "U": "Ulrich",
+        "V": "Viktor",
+        "W": "Wilhelm",
+        "X": "Xanthippe",
+        "Y": "Ypsilon",
+        "Z": "Zacharias",
+        "Ä": "Ärger",
+        "Ö": "Ökonom",
+        "Ü": "Übermut",
+        "0": "Null",
+        "1": "Eins",
+        "2": "Zwei",
+        "3": "Drei",
+        "4": "Vier",
+        "5": "Fünf",
+        "6": "Sechs",
+        "7": "Sieben",
+        "8": "Acht",
+        "9": "Neun",
+        " ": "Leerzeichen",
+        ".": "Punkt",
+        ",": "komma",
+        ";": "Semikolon",
+        ":": "DoppelPunkt",
+        "?": "Fragezeichen",
+        "!": "Ausrufezeichen",
+        "@": "At-Zeichen",
+        "&": "Und-Zeichen",
+        '"': "Anführungszeichen",
+        "'": "Apostroph",
+        "-": "Bindestrich",
+        "/": "Schrägstrich",
+        "\\": "Umgekehrter-Schrägstrich",
+        "(": "Runde-Klammer-links",
+        ")": "Runde-Klammer-rechts",
+        "[": "Eckige-Klammer-links",
+        "]": "Eckige-Klammer-rechts",
+        "{": "Geschweifte-Klammer-links",
+        "}": "Geschweifte-Klammer-rechts",
+        "<": "Kleiner-als-Zeichen",
+        ">": "Größer-als-Zeichen",
+        "|": "Senkrechter-Strich",
+        "°": "Gradzeichen",
+        "*": "Sternchen",
+        "+": "Pluszeichen",
+        "=": "Gleichheitszeichen",
+        "#": "Rautenzeichen",
+        "§": "Paragraphenzeichen",
+        "$": "Dollarzeichen",
+        "€": "Euro-Zeichen",
+        "~": "Tilde",
+        "_": "Unterstrich",
+        "%": "Prozentzeichen",
+        "^": "Zirkumflex",
     }
 
     inter_spelling = {
-        "A":   "Alfa",
-        "B":   "Bravo",
-        "C":   "Charlie",
-        "D":   "Delta",
-        "E":   "Echo",
-        "F":   "Foxtrot",
-        "G":   "Golf",
-        "H":   "Hotel",
-        "I":   "India",
-        "J":   "Juliett",
-        "K":   "Kilo",
-        "L":   "Lima",
-        "M":   "Mike",
-        "N":   "November",
-        "O":   "Oscar",
-        "P":   "Papa",
-        "Q":   "Quebec",
-        "R":   "Romeo",
-        "S":   "Sierra",
-        "T":   "Tango",
-        "U":   "Uniform",
-        "V":   "Victor",
-        "W":   "Whiskey",
-        "X":   "X-ray",
-        "Y":   "Yankee",
-        "Z":   "Zulu",
-
-        "0":   "Zero",
-        "1":   "One",
-        "2":   "Two",
-        "3":   "Three",
-        "4":   "Four",
-        "5":   "Five",
-        "6":   "Six",
-        "7":   "Seven",
-        "8":   "Eight",
-        "9":   "Nine",
-
-        " ":   "Space",
-        ".":   "Dot",
-        ",":   "Comma",
-        ";":   "Semicolon",
-        ":":   "Colon",
-        "?":   "Question-Mark",
-        "!":   "Exclamation-Mark",
-        "@":   "At-Sign",
-        "&":   "Ampersand",
-        "\"":  "Double-Quotation-Mark",
-        "\'":  "Single-Quotation-Mark",
-        "-":   "Minus-Sign",
-        "/":   "Forward-Slash",
-        "\\":  "Backslash",
-        "(":   "Left-Round-Bracket",
-        ")":   "Right-Round-Bracket",
-        "[":   "Left-Square-Bracket",
-        "]":   "Right-Square Bracket",
-        "{":   "Left-Curly-Bracket",
-        "}":   "Right-Curly-Bracket",
-        "<":   "Less-Than-Sign",
-        ">":   "Greater-Than-Sign",
-        "|":   "Pipe",
-        "°":   "Degree-Symbol",
-        "*":   "Star",
-        "+":   "Plus-Sign",
-        "=":   "Equal-Sign",
-        "#":   "Hash",
-        "§":   "Section-Sign",
-        "$":   "Dollar-Sign",
-        "€":   "Euro-Sign",
-        "~":   "Tilde",
-        "_":   "Underscore",
-        "%":   "Percent-Sign",
-        "^":   "Caret",
-
+        "A": "Alfa",
+        "B": "Bravo",
+        "C": "Charlie",
+        "D": "Delta",
+        "E": "Echo",
+        "F": "Foxtrot",
+        "G": "Golf",
+        "H": "Hotel",
+        "I": "India",
+        "J": "Juliett",
+        "K": "Kilo",
+        "L": "Lima",
+        "M": "Mike",
+        "N": "November",
+        "O": "Oscar",
+        "P": "Papa",
+        "Q": "Quebec",
+        "R": "Romeo",
+        "S": "Sierra",
+        "T": "Tango",
+        "U": "Uniform",
+        "V": "Victor",
+        "W": "Whiskey",
+        "X": "X-ray",
+        "Y": "Yankee",
+        "Z": "Zulu",
+        "0": "Zero",
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+        " ": "Space",
+        ".": "Dot",
+        ",": "Comma",
+        ";": "Semicolon",
+        ":": "Colon",
+        "?": "Question-Mark",
+        "!": "Exclamation-Mark",
+        "@": "At-Sign",
+        "&": "Ampersand",
+        '"': "Double-Quotation-Mark",
+        "'": "Single-Quotation-Mark",
+        "-": "Minus-Sign",
+        "/": "Forward-Slash",
+        "\\": "Backslash",
+        "(": "Left-Round-Bracket",
+        ")": "Right-Round-Bracket",
+        "[": "Left-Square-Bracket",
+        "]": "Right-Square Bracket",
+        "{": "Left-Curly-Bracket",
+        "}": "Right-Curly-Bracket",
+        "<": "Less-Than-Sign",
+        ">": "Greater-Than-Sign",
+        "|": "Pipe",
+        "°": "Degree-Symbol",
+        "*": "Star",
+        "+": "Plus-Sign",
+        "=": "Equal-Sign",
+        "#": "Hash",
+        "§": "Section-Sign",
+        "$": "Dollar-Sign",
+        "€": "Euro-Sign",
+        "~": "Tilde",
+        "_": "Underscore",
+        "%": "Percent-Sign",
+        "^": "Caret",
     }
-    spelling_dict = {
-        "German": german_spelling,
-        "Inter": inter_spelling
-    }
+    spelling_dict = {"German": german_spelling, "Inter": inter_spelling}
 
     spell_words = []
     for char in word:
@@ -225,16 +211,17 @@ If you have any ideas, requests or bugs, don´t hasitate to contact me!
 Thx for using my bot! Hope you will enjoy it!
 - @mitschix
     """
-    context.bot.send_message(
-        chat_id=update.message.chat_id, text=start_message)
+    context.bot.send_message(chat_id=update.message.chat_id, text=start_message)
 
 
 # show different menus
 def spelling_start(update, context) -> None:
     reply_markup = InlineKeyboardMarkup(keyboard_main)
-    context.bot.send_message(chat_id=update.message.chat_id,
-                             text="Which language do you prefer?",
-                             reply_markup=reply_markup)
+    context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Which language do you prefer?",
+        reply_markup=reply_markup,
+    )
 
 
 def handle_words(update, context) -> int:
@@ -242,15 +229,18 @@ def handle_words(update, context) -> int:
     given_string = update.message.text
     output = get_mapping(given_string, LANG[user])
     msg = "{} would be:\n\n{}\n\n\nWhat would you like to do next?".format(
-        given_string, output)
-    context.bot.send_message(chat_id=user, text=msg,
-                             reply_markup=InlineKeyboardMarkup(keyboard_after))
+        given_string, output
+    )
+    context.bot.send_message(
+        chat_id=user, text=msg, reply_markup=InlineKeyboardMarkup(keyboard_after)
+    )
     return ConversationHandler.END
 
 
 def wrong_conv(bot, update, user_data) -> None:
-    bot.send_message(chat_id=update.message.chat_id,
-                     text="sorry.. the given value is wrong!")
+    bot.send_message(
+        chat_id=update.message.chat_id, text="sorry.. the given value is wrong!"
+    )
 
 
 def buttons_control(update, context) -> int:
@@ -272,11 +262,14 @@ def buttons_control(update, context) -> int:
         return INTER
     elif option == "again":
         msg = "OK, lets try again.\n\nWhich spelling do you want to choose?"
-        context.bot.send_message(chat_id=user, text=msg,
-                                 reply_markup=InlineKeyboardMarkup(keyboard_main))
+        context.bot.send_message(
+            chat_id=user, text=msg, reply_markup=InlineKeyboardMarkup(keyboard_main)
+        )
         return 0
     else:
-        msg = "Thank you for the usage! If you need me again, just click /howtospell. (:"
+        msg = (
+            "Thank you for the usage! If you need me again, just click /howtospell. (:"
+        )
         context.bot.send_message(chat_id=user, text=msg)
         return ConversationHandler.END
 
@@ -300,9 +293,9 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     # handler
-    start_handler = CommandHandler('start', start)
-    help_handler = CommandHandler('help', help_output)
-    main_handler = CommandHandler('howtospell', spelling_start)
+    start_handler = CommandHandler("start", start)
+    help_handler = CommandHandler("help", help_output)
+    main_handler = CommandHandler("howtospell", spelling_start)
     button_handler = CallbackQueryHandler(buttons_control)
 
     conv_handler = ConversationHandler(
@@ -328,6 +321,6 @@ def main() -> None:
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
     print("User forced me to stop... i am sorrey ):")
